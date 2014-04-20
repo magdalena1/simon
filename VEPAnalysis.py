f024e41c137619f02166d861afa046110beab6a4
Magdalena Zieleniewska <magda.zieleniewska@gmail.com>
2014-04-20 13:35:33 +0200

    normalized dict with signal

--- VEPAnalysis.py
+++ VEPAnalysis.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function, division
+
 import matplotlib.pyplot as py
 import numpy as np
 from rys_10_20 import rys
@@ -15,6 +17,7 @@ class VEPAnalysis(object):
 
 		public interface:
 		draw_VEPs()
+		find_p300(channel)
 
 		"""
 		super(VEPAnalysis, self).__init__()
@@ -25,7 +28,14 @@ class VEPAnalysis(object):
 		self.fs = fs
 
 		self.signal_ind_mean, self.signal_group_mean = self._estimate_mean_VEPs()
-		# self.draw_VEPs(self.channels)
+		self.signal = self._normalize_dict()
+
+	def _normalize_dict(self):
+		signal = defaultdict(dict)
+		for i in self.signal_ind_mean.keys():
+			for j in self.signal_ind_mean[i].keys():
+				signal[i][j] = {'individual': self.signal_ind_mean[i][j], 'group': self.signal_group_mean[i][j]}
+		return signal
 
 	def _estimate_mean_VEPs(self):
 		signal_ind_mean = defaultdict(dict)
@@ -36,57 +46,75 @@ class VEPAnalysis(object):
 				signal_group_mean[channel][condition] = [np.mean(self.signal_group[channel][condition], axis=0),np.var(self.signal_group[channel][condition], axis=0)/len(self.signal_group[channel][condition])] 
 		return signal_ind_mean, signal_group_mean
 
-	def find_p300(self, channel):
-		p300 = dict()
-		p300_ind = self._find_p300_amplitude(channel, self.signal_ind_mean)
-		p300_group = self._find_p300_amplitude(channel, self.signal_group_mean)
+	def find_p300_n100(self, channel):
+		p300 = defaultdict(dict)
+		n100 = defaultdict(dict)
+		p300_ind,n100_ind = self._find_veps_amplitude(channel, self.signal_ind_mean)
+		p300_group,n100_group = self._find_veps_amplitude(channel, self.signal_group_mean)
 		for condition in self.conditions:
-			p300[condition] = [p300_ind[condition], p300_group[condition]]
-		return p300
+			p300[condition] = {'individual':p300_ind[condition],'group':p300_group[condition]}
+			n100[condition] = {'individual':n100_ind[condition],'group':n100_group[condition]}
+		return p300,n100
 
-	def _find_p300_amplitude(self, channel, signal):
+	def _find_veps_amplitude(self, channel, signal):
 		P3_amplitudes = defaultdict(dict)
+		N1_amplitudes = defaultdict(dict)
 		for condition in self.conditions:
 			deriv = np.gradient(np.array(signal[channel][condition][0][:0.5*self.fs]))
 			ids = []
 			amps = []
+			amps_N1 = []
 			for i in xrange(len(deriv)):
 				if i != (0,1):
 					if (deriv[i-1] > 0 and deriv[i] < 0):
 						ids.append(('max',i-1))
 					elif (deriv[i-1] < 0 and deriv[i] > 0):
 						ids.append(('min',i-1))
+			py.figure()
+			py.plot(signal[channel][condition][0])
+			for k in ids:
+				py.vlines(k[1],-6,10,colors='m')
+			py.xlim(0,0.5*1024)
 			for i in xrange(len(ids)):
 				if i != 0:
 					if (ids[i-1][0] == 'min' and ids[i][0] == 'max'):
 						amps.append([abs(signal[channel][condition][0][ids[i][1]]-signal[channel][condition][0][ids[i-1][1]]),
 								    signal[channel][condition][1][ids[i][1]]+signal[channel][condition][1][ids[i-1][1]]])
+					if (ids[i-1][0] == 'max' and ids[i][0] == 'min'):
+						if (ids[i][1] < 250 and ids[i-1][1] > 40):
+							amps_N1.append([abs(signal[channel][condition][0][ids[i][1]]-signal[channel][condition][0][ids[i-1][1]]),
+								    signal[channel][condition][1][ids[i][1]]+signal[channel][condition][1][ids[i-1][1]]])
 			max_id = np.array(amps).argmax(axis=0)[0]
 			P3_amplitudes[condition] = amps[max_id]
-		return P3_amplitudes
+			max_id_n1 = np.array(amps_N1).argmax(axis=0)[0]
+			print ('******************************',condition, amps_N1[max_id_n2])
+			N1_amplitudes[condition] = amps_N1[max_id_n2]
+			py.show()
 
-	def draw_VEPs(self, channels):
+		return P3_amplitudes, N1_amplitudes
+
+	def draw_VEPs(self, channels, signal):
 		modes = ['go', 'no-go']
 		t = np.linspace(0,1,self.fs)
 		if isinstance(channels, list):
 			for mode in modes:
 				rys_go = rys(mode+' trials')
 				for ch in channels:
-					r1 = rys_go[ch].plot(t, self.signal_ind_mean[ch]['compatible-'+mode][0], 'r')
-					rys_go[ch].fill_between(t, self.signal_ind_mean[ch]['compatible-'+mode][0]+self.signal_ind_mean[ch]['compatible-'+mode][1],
-											   self.signal_ind_mean[ch]['compatible-'+mode][0]-self.signal_ind_mean[ch]['compatible-'+mode][1],
+					r1 = rys_go[ch].plot(t, signal[ch]['compatible-'+mode]['individual'][0], 'r')
+					rys_go[ch].fill_between(t, signal[ch]['compatible-'+mode]['individual'][0]+signal[ch]['compatible-'+mode]['individual'][1],
+											   signal[ch]['compatible-'+mode]['individual'][0]-signal[ch]['compatible-'+mode]['individual'][1],
 											   facecolor='r', alpha=0.5)
-					r2 = rys_go[ch].plot(t, self.signal_group_mean[ch]['compatible-'+mode][0], 'g')
-					rys_go[ch].fill_between(t, self.signal_group_mean[ch]['compatible-'+mode][0]+self.signal_group_mean[ch]['compatible-'+mode][1],
-											   self.signal_group_mean[ch]['compatible-'+mode][0]-self.signal_group_mean[ch]['compatible-'+mode][1],
+					r2 = rys_go[ch].plot(t, signal[ch]['compatible-'+mode]['group'][0], 'g')
+					rys_go[ch].fill_between(t, signal[ch]['compatible-'+mode]['group'][0]+signal[ch]['compatible-'+mode]['group'][1],
+											   signal[ch]['compatible-'+mode]['group'][0]-signal[ch]['compatible-'+mode]['group'][1],
 											   facecolor='g', alpha=0.5)
-					r3 = rys_go[ch].plot(t, self.signal_ind_mean[ch]['incompatible-'+mode][0], 'c')
-					rys_go[ch].fill_between(t, self.signal_ind_mean[ch]['incompatible-'+mode][0]+self.signal_ind_mean[ch]['incompatible-'+mode][1],
-											   self.signal_ind_mean[ch]['incompatible-'+mode][0]-self.signal_ind_mean[ch]['incompatible-'+mode][1],
+					r3 = rys_go[ch].plot(t, signal[ch]['incompatible-'+mode]['individual'][0], 'c')
+					rys_go[ch].fill_between(t, signal[ch]['incompatible-'+mode]['individual'][0]+signal[ch]['incompatible-'+mode]['individual'][1],
+											   signal[ch]['incompatible-'+mode]['individual'][0]-signal[ch]['incompatible-'+mode]['individual'][1],
 											   facecolor='c', alpha=0.5)
-					r4 = rys_go[ch].plot(t, self.signal_group_mean[ch]['incompatible-'+mode][0], 'm')
-					rys_go[ch].fill_between(t, self.signal_group_mean[ch]['incompatible-'+mode][0]+self.signal_group_mean[ch]['incompatible-'+mode][1],
-											   self.signal_group_mean[ch]['incompatible-'+mode][0]-self.signal_group_mean[ch]['incompatible-'+mode][1],
+					r4 = rys_go[ch].plot(t, signal[ch]['incompatible-'+mode]['group'][0], 'm')
+					rys_go[ch].fill_between(t, signal[ch]['incompatible-'+mode]['group'][0]+signal[ch]['incompatible-'+mode]['group'][1],
+											   signal[ch]['incompatible-'+mode]['group'][0]-signal[ch]['incompatible-'+mode]['group'][1],
 											   facecolor='m', alpha=0.5)
 					for r in [r1, r2]:
 						py.setp(r,linewidth=2)
@@ -98,27 +126,28 @@ class VEPAnalysis(object):
 				rys_go['legend'].plot([],[],'m',label='Group Incompatible')
 				rys_go['legend'].legend()
 				# py.savefig('./rys/'+mode+'.png')#, bbox_inches='tight')
-		else:
+		else: 
 			for mode in modes:
 				fig = py.figure()
 				py.title(mode+' trials')
-				r1 = py.plot(t, self.signal_ind_mean[channels]['compatible-'+mode][0], 'r', label='Individual Compatible')
-				py.fill_between(t, self.signal_ind_mean[channels]['compatible-'+mode][0]+self.signal_ind_mean[channels]['compatible-'+mode][1],
-								self.signal_ind_mean[channels]['compatible-'+mode][0]-self.signal_ind_mean[channels]['compatible-'+mode][1],
+				r1 = py.plot(t, signal[channels]['compatible-'+mode]['individual'][0], 'r', label='Individual Compatible')
+				py.fill_between(t, signal[channels]['compatible-'+mode]['individual'][0]+signal[channels]['compatible-'+mode]['individual'][1],
+								signal[channels]['compatible-'+mode]['individual'][0]-signal[channels]['compatible-'+mode]['individual'][1],
 								facecolor='r', alpha=0.5)
-				r2 = py.plot(t, self.signal_group_mean[channels]['compatible-'+mode][0], 'g', label='Group Compatible')
-				py.fill_between(t, self.signal_group_mean[channels]['compatible-'+mode][0]+self.signal_group_mean[channels]['compatible-'+mode][1],
-								self.signal_group_mean[channels]['compatible-'+mode][0]-self.signal_group_mean[channels]['compatible-'+mode][1],
+				r2 = py.plot(t, signal[channels]['compatible-'+mode]['group'][0], 'g', label='Group Compatible')
+				py.fill_between(t, signal[channels]['compatible-'+mode]['group'][0]+signal[channels]['compatible-'+mode]['group'][1],
+								signal[channels]['compatible-'+mode]['group'][0]-signal[channels]['compatible-'+mode]['group'][1],
 								facecolor='g', alpha=0.5)
-				r3 = py.plot(t, self.signal_ind_mean[channels]['incompatible-'+mode][0], 'c', label='Individual Incompatible')
-				py.fill_between(t, self.signal_ind_mean[channels]['incompatible-'+mode][0]+self.signal_ind_mean[channels]['incompatible-'+mode][1],
-								self.signal_ind_mean[channels]['incompatible-'+mode][0]-self.signal_ind_mean[channels]['incompatible-'+mode][1],
+				r3 = py.plot(t, signal[channels]['incompatible-'+mode]['individual'][0], 'c', label='Individual Incompatible')
+				py.fill_between(t, signal[channels]['incompatible-'+mode]['individual'][0]+signal[channels]['incompatible-'+mode]['individual'][1],
+								signal[channels]['incompatible-'+mode]['individual'][0]-signal[channels]['incompatible-'+mode]['individual'][1],
 								facecolor='c', alpha=0.5)
-				r4 = py.plot(t, self.signal_group_mean[channels]['incompatible-'+mode][0], 'm', label='Group Incompatible')
-				py.fill_between(t, self.signal_group_mean[channels]['incompatible-'+mode][0]+self.signal_group_mean[channels]['incompatible-'+mode][1],
-								self.signal_group_mean[channels]['incompatible-'+mode][0]-self.signal_group_mean[channels]['incompatible-'+mode][1],
+				r4 = py.plot(t, signal[channels]['incompatible-'+mode]['group'][0], 'm', label='Group Incompatible')
+				py.fill_between(t, signal[channels]['incompatible-'+mode]['group'][0]+signal[channels]['incompatible-'+mode]['group'][1],
+								signal[channels]['incompatible-'+mode]['group'][0]-signal[channels]['incompatible-'+mode]['group'][1],
 								facecolor='m', alpha=0.5)
 				for r in [r1, r2]:
 					py.setp(r,linewidth=2)
+				py.xlim(0,0.6)
 				py.legend()
 		py.show()
--- vep_calculation_v2.py
+++ vep_calculation_v2.py
@@ -12,9 +12,11 @@ import os.path, ast
 from scipy.signal import filtfilt, cheby2, butter, resample
 import random
 from collections import defaultdict
+import csv
+from VEPAnalysis import *
 
-class VEPAnalysis(object):
-	def __init__(self, file_path, f_name_ind, f_name_group):
+class VisualEvokedPotentials(object):
+	def __init__(self, file_path, f_name_ind, f_name_group, rts_ind, rts_group, get_rts=0):
 		"""
 		format zwracanych danych frags['O1']['compatible-go'][[1024],[1024],[1024],...]
 		warunki ['compatible-go','compatible-no-go','incompatible-go','incompatible-no-go']
@@ -29,12 +31,15 @@ class VEPAnalysis(object):
 		public interface:
 
 		"""
-		super(VEPAnalysis, self).__init__()
+		super(VisualEvokedPotentials, self).__init__()
 		self.channels = [u'Fp1',u'Fpz',u'Fp2',u'F7',u'F3',u'Fz',u'F4',u'F8',u'T3',u'C3',u'Cz',u'C4',u'T4',u'T5', 
 		  u'P3', u'Pz', u'P4', u'T6', u'O1', u'Oz', u'O2']
-
 		self.frags_group = self._cut_fragments(os.path.join(os.path.normpath(file_path), f_name_group))
+		if get_rts:
+			self.rts_group = self._get_RTs(rts_group)
 		self.frags_ind = self._cut_fragments(os.path.join(os.path.normpath(file_path), f_name_ind))
+		if get_rts:
+			self.rts_ind = self._get_RTs(rts_ind)
 
 	def _get_current_condition(self, file_name):
 		f = os.path.basename(file_name).split('_')
@@ -45,9 +50,8 @@ class VEPAnalysis(object):
 		ref_sig = []
 		for i in xrange(23):
 			new_sig = sig[i]-0.5*(self.mgr.get_channel_samples('A1') + self.mgr.get_channel_samples('A2'))
-			ref_sig.append(new_sig - np.mean(new_sig))
-		filtered_sig = self._filter_signal(ref_sig)
-		return filtered_sig
+			ref_sig.append(new_sig - np.mean(new_sig))		
+		return ref_sig
 
 	def _filter_signal(self, signal):
 		sig_f = []
@@ -68,8 +72,8 @@ class VEPAnalysis(object):
 		times = {}
 		for k in keys:
 			times[k] = []
-		reader = TFR.TagsFileReader(tag_file)
-		tags = reader.get_tags()
+		# reader = TFR.TagsFileReader(tag_file)
+		tags = self.mgr.get_tags()
 		proper_cond = [key for key in keys if self.condition in key]
 		wrong_cond = list(set(keys) - set(proper_cond))
 		for tag in tags:
@@ -98,7 +102,7 @@ class VEPAnalysis(object):
 			proper_key = 'left'
 		return proper_key
 
-	def _normalize_keys(self, file_name, frags):
+	def _normalize_keys(self, frags):
 		proper_key = self._get_proper_key()
 		other_key = 'left' if self.condition == 'blue' else 'right'
 		other_condition = 'green' if self.condition == 'blue' else 'blue'
@@ -127,8 +131,9 @@ class VEPAnalysis(object):
 		pointsPerMikroV = self.mgr.get_param('channels_gains')
 		self.fs = float(self.mgr.get_param('sampling_frequency'))
 		# sig = self.mgr.get_samples()		### uncomment if signal is already filtered
-		sig = self._linked_ears_montage()
+		ref_sig = self._linked_ears_montage()
 		# sig = _one_ear_montage(self.mgr, self.fs)
+		sig = self._filter_signal(ref_sig)
 		times = self._get_timestamps(file_name+'.tag')
 		stim = {}
 		for key in times.keys():
@@ -145,11 +150,99 @@ class VEPAnalysis(object):
 					frags[channels[i]][key].append(sig[i][j:j+int(self.fs)]-np.mean(sig[i][j-int(0.1*self.fs):j+int(self.fs)]))
 		frags.pop('A1', None)
 		frags.pop('A2', None)
-		frags = self._normalize_keys(file_name,frags)
+		frags = self._normalize_keys(frags)
 		return frags
+
+	def _compute_mean_RTs(self, rts):
+		mean_rts = {}
+		for key in rts.keys():
+			data = self._reject_outliers(rts[key])
+			# print(len(data))
+			# print(key,'->',list(set(rts[key]) - set(data)))	### show differences between lists
+			mean_rts[key] = [round(np.mean(data),4), round(np.std(data),4)]
+		return mean_rts
+
+	def _reject_outliers(self, data, m=8.):
+		d = np.abs(data-np.median(data))
+		mdev = np.median(d)
+		norm_data = d/mdev
+		new_data = [data[i] for i,j in enumerate(norm_data) if j<m]
+		return new_data
+
+	def _normalize_RTs_keys(self, data):
+		proper_key = self._get_proper_key()
+		other_key = 'left' if self.condition == 'blue' else 'right'
+		other_condition = 'green' if self.condition == 'blue' else 'blue'
+		data_norm = defaultdict()
+		for key in data.keys():
+			if key == self.condition+'-'+proper_key:
+				data_norm['compatible-go'] = data[key]
+			elif key == self.condition+'-'+other_key:
+				data_norm['incompatible-go'] = data[key]
+		return data_norm
+
+	def _get_RTs(self, csv_file):
+		proper_key = self._get_proper_key()
+		keys = [self.condition+'-left', self.condition+'-right']
+		rts = {}
+		for k in keys:
+			rts[k] = []
+		tags = self.mgr.get_tags()
+		for tag in tags:
+			if tag['name'] in rts and str(tag['desc']['key']) != 'None':
+				if isinstance(tag['desc']['key'], basestring) and tag['desc']['key'] == proper_key:
+					rts[tag['name']].append(float(tag['desc']['response_time']))
+				else:
+					try:
+						k = ast.literal_eval(tag['desc']['key'])
+						rt = ast.literal_eval(tag['desc']['response_time'])
+						ids = [i for i,j in enumerate(k) if j == proper_key]
+						rts[tag['name']].append(float(rt[ids[0]]))
+					except (TypeError, ValueError) as e:
+						print(e)
+		mean_rts = self._compute_mean_RTs(rts)
+		mean_rts = self._normalize_RTs_keys(mean_rts)
+		self._write_csv(csv_file, mean_rts)
+		return mean_rts
+
+	def _write_csv(self, csv_file, mean_rts):
+		if not os.path.isfile(csv_file):
+			with open(csv_file, 'wb') as f:
+				csv.writer(f).writerows([k,] + v for k, v in mean_rts.iteritems())
+		else:
+			with open(csv_file, 'a') as f:
+				csv.writer(f).writerows([k,] + v for k, v in mean_rts.iteritems())
 
 if __name__ == '__main__':
 	path = './badania_part3/'
-	# f_list_group = ['21_K_RB_01']
-	# f_list_ind = ['21_K_R_01']
-	v = VEPAnalysis(path,'23_K_R_01','23_K_RB_01')
+	rts_group = './RTs_group.csv'
+	rts_ind = './RTs_ind.csv'
+	f_list_group = ['15_K_RB_01']
+	f_list_ind = ['15_K_R_01']
+	# f_list_group = ['28_K_LB_01','23_K_RB_01','32_K_LB_01','11_K_RB_01','09_M_RB_01','25_K_RB_01','18_K_LB_01']
+	# f_list_ind = ['28_K_L_01','23_K_R_01','32_K_L_01','11_K_R_01','09_M_R_01','25_K_R_01','18_K_L_01']
+
+	if len(f_list_group) == 1:
+		vep = VisualEvokedPotentials(path,f_list_ind[0],f_list_group[0],rts_ind,rts_group,get_rts=0)
+		v = VEPAnalysis(vep.frags_ind,vep.frags_group,vep.fs)
+		v.draw_VEPs(v.channels,v.signal)
+	else:
+		grand_av = defaultdict(dict)
+		for i in xrange(len(f_list_ind)):
+			vep = VisualEvokedPotentials(path,f_list_ind[i],f_list_group[i],rts_ind,rts_group,get_rts=0)
+			v = VEPAnalysis(vep.frags_ind,vep.frags_group,vep.fs)
+			if i == 0:
+				for j in v.signal.keys():
+					for k in v.signal[j].keys():
+						grand_av[j][k] = {'individual': [], 'group': []}
+			for j in v.signal.keys():
+				for k in v.signal[j].keys():
+					grand_av[j][k]['individual'].append(v.signal[j][k]['individual'])
+					grand_av[j][k]['group'].append(v.signal[j][k]['group'])
+		for j in grand_av.keys():
+			for k in grand_av[j].keys():
+				for l in grand_av[j][k].keys():
+					grand_av[j][k][l] = np.mean(grand_av[j][k][l],axis=0)
+		v.draw_VEPs(v.channels,grand_av)
+	# p,n = v.find_p300_n100('Oz')
+
